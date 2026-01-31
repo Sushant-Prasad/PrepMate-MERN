b0e5a3c192410517c35ebb6971e1ea1c2c4d463b
Sushant-Prasad <sushantprasad77@gmail.com>
2026-01-31 19:23:57 +0530

    refactor file

--- Server/models/Message.js
+++ Server/models/Message.js
@@ -1,24 +1,78 @@
 import mongoose from "mongoose";
 
+
 const messageSchema = new mongoose.Schema(
   {
-    roomId: { type: mongoose.Schema.Types.ObjectId, ref: "StudyRoom" }, // null for direct messages
-    sender: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "UserProfile",
-      required: true,
+    conversation: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "Conversation" 
     },
-    receiver: { type: mongoose.Schema.Types.ObjectId, ref: "UserProfile" }, // only for direct messages
-    type: {
+    group: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "Group" 
+    },
+    sender: { 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "User", 
+      required: true 
+    },
+    body: { 
+      type: String, 
+      trim: true 
+    },
+    mediaUrl: { 
+      type: String 
+    },
+    mediaType: { 
+      type: String, 
+      enum: ["image", "video", "file"] 
+    },
+    content: {
       type: String,
-      enum: ["text", "file", "image", "pdf", "link"],
-      default: "text",
+      trim: true,
     },
-    content: { type: String, required: true }, // message text OR file URL
-    fileName: { type: String },
-    fileType: { type: String },
+    readBy: [{ 
+      type: mongoose.Schema.Types.ObjectId, 
+      ref: "User" 
+    }],
   },
   { timestamps: true }
 );
 
+
 export default mongoose.model("Message", messageSchema);
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+
+// const messageSchema = new mongoose.Schema(
+//   {
+//     roomId: { type: mongoose.Schema.Types.ObjectId, ref: "StudyRoom" }, // null for direct messages
+//     sender: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "UserProfile",
+//       required: true,
+//     },
+//     receiver: { type: mongoose.Schema.Types.ObjectId, ref: "UserProfile" }, // only for direct messages
+//     type: {
+//       type: String,
+//       enum: ["text", "file", "image", "pdf", "link"],
+//       default: "text",
+//     },
+//     content: { type: String, required: true }, // message text OR file URL
+//     fileName: { type: String },
+//     fileType: { type: String },
+//   },
+//   { timestamps: true }
+// );
+
+// export default mongoose.model("Message", messageSchema);
+
+
+
+
